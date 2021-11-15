b40ed4ca1ced8842889acaad885863d5d252bfff
hamzaseif13 <66957751+hamzaseif13@users.noreply.github.com>
2021-11-15 13:31:34 +0200

    first commit

---
+++ Game/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.StartTimer = new System.Windows.Forms.Timer(this.components);
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.TotalLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(759, 38);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(184, 34);
+            this.StartBtn.TabIndex = 1;
+            this.StartBtn.Text = "Start game";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // StartTimer
+            // 
+            this.StartTimer.Interval = 1;
+            this.StartTimer.Tick += new System.EventHandler(this.StartTimer_Tick);
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ScoreLabel.Location = new System.Drawing.Point(32, 38);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(86, 31);
+            this.ScoreLabel.TabIndex = 2;
+            this.ScoreLabel.Text = "label1";
+            // 
+            // TotalLabel
+            // 
+            this.TotalLabel.AutoSize = true;
+            this.TotalLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.TotalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TotalLabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.TotalLabel.Location = new System.Drawing.Point(133, 38);
+            this.TotalLabel.Name = "TotalLabel";
+            this.TotalLabel.Size = new System.Drawing.Size(86, 31);
+            this.TotalLabel.TabIndex = 3;
+            this.TotalLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(1034, 661);
+            this.Controls.Add(this.TotalLabel);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.StartBtn);
+            this.MaximumSize = new System.Drawing.Size(1050, 700);
+            this.MinimumSize = new System.Drawing.Size(1050, 700);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Timer StartTimer;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Label TotalLabel;
+    }
+}
+
---
+++ Game/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+        int Score = 0;
+        int Ticks = 0;
+        int Total = 0;
+        List<Projectile> Projectiles;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Projectiles = new List<Projectile>();
+
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            StartTimer.Enabled = true;
+ 
+        }
+
+        private void StartTimer_Tick(object sender, EventArgs e)
+        {
+
+            if (Ticks % 75 == 0)
+            {
+                Projectile drop = new Projectile(true);
+                Projectiles.Add(drop);
+                this.Controls.Add(drop.Pic);
+                Total++;
+                TotalLabel.Text = Total.ToString();
+            }
+            for (int j = 0; j < Projectiles.Count; j++)
+            {
+                Projectiles[j].move();
+                if (Projectiles[j].Pic.Top == 700)
+                {
+                    if (Projectiles[j].IsFruit) Score -= 10;
+                    ScoreLabel.Text = Score.ToString();
+                    Projectiles[j].Pic.Visible = false;
+                    Projectiles.RemoveAt(j);
+                    j--;
+                }
+            }
+            Ticks++;
+        }
+
+
+    }
+    class Projectile
+    {
+        public PictureBox Pic;
+        public bool IsFruit;
+        public Projectile(bool IsFruit)
+        {
+            this.IsFruit = IsFruit;
+            string current;
+            string bomb = "bomb.png";
+            string[] imgs = new string[] { "banana.png", "apple.png", "strawberry.png" };
+
+            Random rand = new Random();
+            if (this.IsFruit)
+            {
+                current = imgs[rand.Next(0, 3)];
+            }
+            else
+            {
+                current = bomb;
+            }
+            Pic = new PictureBox()
+            {
+                Size = new Size(75, 75),
+                Location = new Point(150 * rand.Next(0, 7), 0),
+                ImageLocation = @"C:\Users\hamza\Desktop\project-assest\" + current,
+                Name = "hamza",
+                SizeMode = PictureBoxSizeMode.StretchImage
+            };
+
+        }
+        public void move()
+        {
+            Pic.Top += 2;
+        }
+
+    }
+
+}
